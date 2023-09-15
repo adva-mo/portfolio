f25ffdd73773b5e7034f26ffc9658f440b3801e5
adva-mo <advamozes@gmail.com>
2023-09-15 21:08:53 +0300

    Homepage design

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="">
-      <p>hello </p>
+    <main className="homepage">
+      <section className="grid p-20 h-full text-black">
+        <h2 className="text-2xl">ADVA MOZES</h2>
+        <h3 className="text-4xl">
+          Hello! I'm Adva, a backend <br />
+          developer from israel.
+        </h3>
+        <p>thank you for visit my page, check out my cool projects</p>
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      black: "rgb(39, 34, 38)",
     },
   },
   plugins: [],
-}
+};
