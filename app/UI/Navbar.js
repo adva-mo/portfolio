ec81e3d5aa4da68925949294c1e9b5a4fe7d9e71
adva-mo <advamozes@gmail.com>
2023-09-16 17:32:48 +0300

    navbar design

--- app/UI/Navbar.js
+++ app/UI/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="flex gap-3 ">
+    <div className="flex gap-6 text-gray-600/50 uppercase text-xs">
       <Link href="/content/backend">Backend </Link>
       <Link href="/content/e2e">e2e </Link>
       <Link href="/content/about">about </Link>
