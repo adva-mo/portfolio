f23912a62e091e967df90624b85240230ac12d43
adva-mo <advamozes@gmail.com>
2023-09-22 11:59:41 +0300

    Contact page design

--- app/(content)/contact/page.js
+++ app/(content)/contact/page.js
@@ -1,17 +1,11 @@
 "use client";
+import Image from "next/image";
 
 export default function page() {
   return (
-    <div class="max-w-screen-xl px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
-      <div class="flex flex-col justify-between">
-        <div>
-          <h2 class="text-4xl lg:text-5xl font-bold leading-tight">
-            Lets talk about everything!
-          </h2>
-          <div class="text-gray-700 mt-8">
-            Hate forms? Send us an <span class="underline">email</span> instead.
-          </div>
-        </div>
+    <div class="max-w-screen-xl px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg mt-10">
+      <div class="flex flex-col justify-between relative w-[40vw]">
+        <Image src="/contact-img.png" fill={true} className="rounded"></Image>
       </div>
       <div class="">
         <div>
@@ -33,7 +27,7 @@ export default function page() {
         </div>
         <div class="mt-8">
           <span class="uppercase text-sm text-gray-600 font-bold">Message</span>
-          <textarea class="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+          <textarea class="w-full h-36 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline resize-none"></textarea>
         </div>
         <div class="mt-8">
           <button class="uppercase text-sm font-bold tracking-wide bg-sand text-black p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
