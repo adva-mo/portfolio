18b59a02a15605c6debbb59738c46886b062712e
adva-mo <advamozes@gmail.com>
2023-09-22 11:40:01 +0300

    Convert content path to be hidden

--- app/UI/Navbar.js
+++ app/UI/Navbar.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="flex gap-6 text-gray-600/50 uppercase text-xs">
-      <Link href="/content/backend">Backend </Link>
-      <Link href="/content/e2e">e2e </Link>
-      <Link href="/content/about">about </Link>
-      <Link href="/content/contact">contact me</Link>
+      <Link href="/backend">Backend </Link>
+      <Link href="/e2e">e2e </Link>
+      <Link href="/about">about </Link>
+      <Link href="/contact">contact me</Link>
     </div>
   );
 }
